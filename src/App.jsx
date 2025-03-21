bb3236a18d2e210c12578083b9cf647414cdb575
Brenda Silva Cardoso Souza <brenda.tads@gmail.com>
2025-03-21 00:59:32 -0300

    Primeiro commit: adicionar todos os arquivos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Importe o arquivo CSS com os estilos
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.jpeg" className="App-logo" alt="logo" />
+        <h1 className="App-title">ArthillesMagno AI</h1>
+        <p className="App-description">Sua solução de inteligência artificial e inovação digital</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
